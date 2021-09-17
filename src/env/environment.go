1d360c3e098eb533754612e47edbbbcf96866a38
Christoph Krassnigg <mail@gebes.eu>
2021-09-17 12:32:46 +0200

    🧹 Cleanup

--- src/env/environment.go
+++ src/env/environment.go
@@ -3,16 +3,12 @@ package env
 import (
 	"github.com/joho/godotenv"
 	"os"
-	"strconv"
 )
 
 var (
 	MySqlDatabase string
 
-	MailServerAddress  string
-	MailServerPort     int
-	MailServerUsername string
-	MailServerPassword string
+
 
 	SendgridApiKey string
 )
@@ -23,15 +19,8 @@ func LoadEnvironment() error {
 	if err != nil {
 		return err
 	}
-	mailServerPort, err := strconv.Atoi(os.Getenv("MAIL_SERVER_PORT"))
-	if err != nil {
-		return err
-	}
+
 	MySqlDatabase = os.Getenv("MYSQL_DATABASE")
-	MailServerAddress = os.Getenv("MAIL_SERVER_ADDRESS")
-	MailServerPort = mailServerPort
-	MailServerUsername = os.Getenv("MAIL_USERNAME")
-	MailServerPassword = os.Getenv("MAIL_PASSWORD")
 	SendgridApiKey = os.Getenv("SENDGRID_API_KEY")
 
 	return nil
